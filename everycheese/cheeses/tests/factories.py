6da33c9a2bf63c353780d039af90a5625e9b8cdc
Muddam Hemasri <hemasri.muddam@unh.edu>
2023-09-29 15:32:37 -0400

    Modify factory to create cheese creator user

--- everycheese/cheeses/tests/factories.py
+++ everycheese/cheeses/tests/factories.py
@@ -2,6 +2,7 @@ from django.template.defaultfilters import slugify
 import factory
 import factory.fuzzy
 from ..models import Cheese
+from everycheese.users.tests.factories import UserFactory
 class CheeseFactory(factory.django.DjangoModelFactory):
     name = factory.fuzzy.FuzzyText()
     slug = factory.LazyAttribute(
@@ -10,7 +11,9 @@ class CheeseFactory(factory.django.DjangoModelFactory):
         'paragraph', nb_sentences=3,
          variable_nb_sentences=True)
     firmness = factory.fuzzy.FuzzyChoice(
-        [x[0] for x in Cheese.Firmness.choices]
-    )
+        [x[0] for x in Cheese.Firmness.choices])
+    country_of_origin = factory.Faker('country_code')
+    creator = factory.SubFactory(UserFactory)
+
     class Meta:
         model = Cheese
