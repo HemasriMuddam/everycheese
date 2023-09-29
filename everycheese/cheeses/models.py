df14481560e924bb16c5a8dfb2fdb46c95f50edc
Muddam Hemasri <hemasri.muddam@unh.edu>
2023-09-29 14:11:59 -0400

    First pass at working Add Cheese form

--- everycheese/cheeses/models.py
+++ everycheese/cheeses/models.py
@@ -1,9 +1,9 @@
-from django.conf import settings
 from django.db import models
 from model_utils.models import TimeStampedModel
 from autoslug import AutoSlugField
 from django_countries.fields import CountryField
 from django.urls import reverse
+from django.conf import settings
 
 
 class Cheese(TimeStampedModel):
@@ -11,7 +11,10 @@ class Cheese(TimeStampedModel):
     slug = AutoSlugField("Cheese Address", unique=True, always_update=False, populate_from="name")
     description = models.TextField("Description", blank=True)
     country_of_origin = CountryField("Country of Origin", blank=True)
-    
+
+
+
+
     class Firmness(models.TextChoices):
         UNSPECIFIED = "unspecified", "Unspecified"
         SOFT = "soft", "Soft"
@@ -22,10 +25,11 @@ class Cheese(TimeStampedModel):
     creator = models.ForeignKey(settings.AUTH_USER_MODEL,null=True,on_delete=models.SET_NULL)
 
 
+
+
     def __str__(self):
         return self.name
+
     def get_absolute_url(self):
         """Return absolute URL to the Cheese Detail page."""
-        return reverse(
-        'cheeses:detail', kwargs={"slug": self.slug}
-        )
+        return reverse('cheeses:detail', kwargs={"slug": self.slug})
--- everycheese/cheeses/views.py
+++ everycheese/cheeses/views.py
@@ -2,15 +2,17 @@ from django.contrib.auth.mixins import LoginRequiredMixin
 from django.views.generic import ListView, DetailView
 from .models import Cheese
 from django.views.generic import CreateView
+
 class CheeseListView(ListView):
     model = Cheese
 class CheeseDetailView(DetailView):
     model = Cheese
 class CheeseCreateView(LoginRequiredMixin, CreateView):
-
     model = Cheese
-    fields = ['name','description','firmness','country_of_origin',]
-    def form_valid(self, form):
-        form.instance.creator = self.request.user
-        return super().form_valid(form)
+    fields = [
+'name',
+'description',
+'firmness',
+'country_of_origin',
+]
 
